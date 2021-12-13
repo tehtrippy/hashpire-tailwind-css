f555cd861eed295b340acb04979912360bf06fd2
Parinya Rueangphun <parinya.rueangphun@gmail.com>
2021-12-13 16:09:34 +0700

    feat: layout content max-width

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -4,9 +4,11 @@ import { MainPageFooter } from '../footer';
 
 const Layout: React.FC<{}> = ({ children }) => {
   return (
-    <div className='min-h-screen flex flex-col bg-primary-light'>
+    <div className='min-h-screen flex flex-col justify-center bg-primary-light'>
       <Navbar />
-      <div className='flex-grow px-4 py-6 md:p-6 lg:p-9'>{children}</div>
+      <div className='flex-grow px-4 py-6 md:p-6 lg:p-9 lg:mx-auto max-w-6xl'>
+        {children}
+      </div>
       <MainPageFooter />
     </div>
   );
